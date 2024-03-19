d7329b7b0f9e1f0ad5d212c8f5ab863ad08b685a
Alextopher <christopher.mahoney2000@gmail.com>
2024-03-19 09:56:46 -0400

    intake: digital flip sensor logic

--- src/main/java/frc/robot/subsystems/GroundIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/GroundIntakeSubsystem.java
@@ -40,7 +40,7 @@ public class GroundIntakeSubsystem extends SubsystemBase {
      * @return true if a note is detected, false otherwise.
      */
     private boolean hasNote() {
-        return irSensor.getVoltage() < 2.5;
+        return irSensor.getVoltage() > 2.5;
     }
 
     /**
