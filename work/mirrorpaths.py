ca34d08f86e884aa89bd20f77f8bf6fcb24b8cd4
Alextopher <christopher.mahoney2000@gmail.com>
2024-03-21 09:24:46 -0400

    autos: red autos

--- work/mirrorpaths.py
+++ work/mirrorpaths.py
@@ -6,13 +6,7 @@ import re
 def modify_json(data, filename):
     if isinstance(data, dict):
         for key, value in data.items():
-            if key == 'x':
-                data[key] = 16.54 - value
-            elif key == 'rotation' or key == 'rotationDegrees':
-                data[key] = (180 - value) % 360
-                if data[key] > 180:
-                    data[key] -= 360
-            elif key == 'folder':
+            if key == 'folder':
                 data[key] = 'RED'
             elif key == 'pathName':
                 data[key] = data[key].replace('BLUE', 'RED')
@@ -22,14 +16,6 @@ def modify_json(data, filename):
         for item in data:
             modify_json(item, filename)
 
-def rotate_end_state(data, filename):
-    if re.match(r'(RED|BLUE)-(M|N|A|B)(\d)?-S.*\.path', filename):
-        # add 90 degrees to the end state rotation
-        # data['goalEndState']['rotation'] = (data['goalEndState']['rotation'] - 45) % 360
-        # if data['goalEndState']['rotation'] > 180:
-        #     data['goalEndState']['rotation'] -= 360
-        pass
-
 # Directory containing JSON files
 directory = '.'
 
@@ -37,6 +23,7 @@ directory = '.'
 for filename in os.listdir(directory):
     if (filename.endswith('.path') or filename.endswith('.auto')) and filename.startswith('BLUE'):
         file_path = os.path.join(directory, filename)
+        print(file_path)
         with open(file_path, 'r') as file:
             # Load JSON data
             json_data = json.load(file)
@@ -44,11 +31,9 @@ for filename in os.listdir(directory):
         # Modify JSON data
         modify_json(json_data, filename)
 
-        rotate_end_state(json_data, filename)
+        # Rename to RED-
+        file_path = file_path.replace("BLUE", "RED")
 
-        # remove "BLUE" from start of filename and write "RED"
-        file_path = file_path.replace('BLUE', 'RED')
-        
         # Write modified JSON data back to the file
         with open(file_path, 'w') as file:
             json.dump(json_data, file, indent=2)
