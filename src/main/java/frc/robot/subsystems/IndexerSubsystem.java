d283d099a3130a721c0dee144a36b86b40387f95
Alextopher <christopher.mahoney2000@gmail.com>
2024-10-27 19:43:17 -0400

    indexer: restore factory defaults before using motor controller

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -7,6 +7,7 @@ import static edu.wpi.first.units.Units.Value;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IndexerConstants;
@@ -17,8 +18,16 @@ public class IndexerSubsystem extends SubsystemBase {
 
     public IndexerSubsystem() {
         indexerMotor = new CANSparkMax(CANConstants.kIndexerMotor, MotorType.kBrushless);
+        indexerMotor.restoreFactoryDefaults();
         indexerMotor.setIdleMode(IdleMode.kBrake);
         indexerMotor.setInverted(true);
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Indexer/Voltage", indexerMotor.getAppliedOutput() * indexerMotor.getBusVoltage());
+        SmartDashboard.putNumber("Indexer/Current", indexerMotor.getOutputCurrent());
+        SmartDashboard.putNumber("Indexer/Velocity", indexerMotor.getEncoder().getVelocity());
     }
 
     private void up() {
